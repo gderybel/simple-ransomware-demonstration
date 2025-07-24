7a50d67f01edff0fbf9438a6938f3808e9059411
Guillaume de Rybel <90607561+gderybel@users.noreply.github.com>
2025-07-24 16:52:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,54 @@
 import os
+import argparse
 from Encryptor import Encryptor
 from Decryptor import Decryptor
 
-choice = input("Would you like to encrypt or decrypt your files ? (e/d) : ")
+if __name__ == "__main__":
+    # Configure parser
+    parser = argparse.ArgumentParser(description="This tool encrypt or decrypt files.",
+                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-if choice == "e":
-    # Encrypt
-    folder = input("Path to encrypt (only files within this folder will be encrypted) : ")
-    extensions = []
-    while True:
-        extension = input("Which extensions would you like to encrypt (enter them one by one) : ")
-        if extension == "":
-            break
-        else:
-            if not extension.startswith('.'):
-                extension = "." + extension
-            extensions.append(extension)
-    encryptor = Encryptor(
-        encrypted_extensions=extensions,
-        encryption_level=16,
-        computer_name=os.getenv('COMPUTERNAME'),
-        folder=folder
-    )
-    encryptor.encrypt_files()
-    # Send the key to a web server for example, to decrypt files later
-    print(encryptor.encryption_key)
-elif choice == "d":
-    # Decrypt
-    encryption_key = input("The encryption key used in encryption process : ")
-    folder = input("Path to decrypt (only files within this folder will be decrypted) : ")
-    extensions = []
-    while True:
-        extension = input("Which extensions would you like to decrypt (enter them one by one) : ")
-        if extension == "":
-            break
-        else:
-            if not extension.startswith('.'):
-                extension = "." + extension
-            extensions.append(extension)
-    decryptor = Decryptor(
-        decryption_key=encryption_key,
-        encrypted_extensions=extensions,
-        encryption_level=16,
-        folder=folder,
-    )
-    decryptor.decrypt_files()
-else:
-    exit("This isn't an known option.")
+    # Add filter on folder to encrypt/decrypt
+    parser.add_argument("-f", "--folder", required=True,
+                        help="which folder should be encrypted/decrypted")
+    # Add encrypt boolean
+    parser.add_argument("-r", "--recursive", action="store_true",
+                        help="tells if the folder selected should be encrypted recursively")
+    # Add decryption key
+    parser.add_argument("-k", "--key",
+                        help="specified decryption key, if given, \
+                            decryption mode is activated, other wise it will be encryption")
+    # Add boolean to not manually check the encryption process
+    parser.add_argument("-y", "--consent", action="store_true",
+                        help="if selected, user won't be prompted for consent before encryption")
+
+    # Get args
+    args = parser.parse_args()
+    folder = args.folder
+    recursive = args.recursive
+    key = args.key
+    consent = args.consent
+
+    if not key:
+        if not consent:
+            answer = input("Are you sure you wanna encrypt your files ? (y) : ")
+            if answer != "y":
+                exit("Abborting, please enter 'y' if you want to encrypt.")
+        # process to encryption
+        encryptor = Encryptor(
+            computer_name=os.uname().nodename if hasattr(os, 'uname') else os.getenv('COMPUTERNAME'),
+            user_name=os.getenv('USER') or os.getenv('USERNAME'),
+            folder=folder,
+            recursive=recursive
+        )
+        encryptor.encrypt_files()
+        # Send the key to a web server for example, to decrypt files later
+        print(f'[{encryptor.computer_name}\\{encryptor.user_name}] -> {encryptor.encryption_key}')
+    else:
+        # process to decryption
+        decryptor = Decryptor(
+            decryption_key=key,
+            folder=folder,
+            recursive=recursive
+        )
+        decryptor.decrypt_files()
